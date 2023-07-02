b01bc404ff60997b8c57368bc806b8ddd2ba7db0
Atarasin <ataraxiazh@gmail.com>
2023-07-02 13:16:40 +0800

    update clangd config to support include search

--- lua/lsp/config/clangd.lua
+++ lua/lsp/config/clangd.lua
@@ -6,6 +6,19 @@ local opts = {
         common.disableFormat(client)
         common.keyAttach(bufnr)
     end,
+    root_pattern = {
+        ".clangd",                              -- clangd配置文件
+        ".clang-tidy",
+        ".clang-format",
+        "compile_commands.json",
+        "compile_flags.txt",
+        "configure.ac",
+        ".git",
+    },
+    cmd = {
+        "clangd",
+        "--query-driver", "/usr/bin/gcc",       -- 指定编译器
+    },
 }
 
 return {
